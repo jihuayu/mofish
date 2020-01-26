78583fa752e3d7982f4721dc9eccd423ca63bf7c
jihuayu <8042833@qq.com>
2020-01-26 21:45:17 +0800

    fix the bug in main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include <example.h>
 int main() {
     std::cout << "I love mofish!" << std::endl;
-    std::cout << "I mofish "<<add_numbers(1,1)<<" times every day!" << std::endl;
+    std::cout << "I mofish "<<add(1,1)<<" times every day!" << std::endl;
     return 0;
 }
